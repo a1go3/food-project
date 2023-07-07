6ea3b89e377144c05cc0f071da3ccd9a6a5a0b43
tatarenkov-r-v <flexomj@gmail.com>
2023-07-07 11:54:38 +0400

    add test`s

--- backend/api/tests/test_api.py
+++ backend/api/tests/test_api.py
@@ -5,36 +5,48 @@ from django.db.models import Count, Case, When, Avg
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-
+from rest_framework.test import APIClient
+from rest_framework.test import RequestsClient
 from food.models import Tag, Ingredient, Recipe, IngredientAmount
-from api.serializers import RecipieWrightSerializers, RecipieReadSerializers
 
 
 class FoodApiTestCase(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create(username='db_user')
+        Tag.objects.create(
+            name='завтрак',
+            color='синий',
+            slug='breakfast'
+        )
+        Ingredient.objects.create(
+            name='яйцо',
+            measurement_unit='шт'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_get(self):
+        url = reverse('api:recipes-list')
+        response = self.client.get(url)
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
 
     def test_create(self):
-        url = reverse('recipes-list')
+        url = reverse('api:recipes-list')
         data = {
-            "tags": [
-                1,
-                2
+            'tags': [
+                1
             ],
-
-            "ingredients": [
+            'ingredients': [
                 {
-                    "id": 1,
-                    "amount": 2
+                    'id': 1,
+                    'amount': 2
                 }
             ],
-            "name": "Яичница-2",
-
-            "text": "Разбей яйца и Пожарь еще",
-            "cooking_time": 10
+            'name': 'Яичница',
+            'text': 'Разбей яйца и пожарь.',
+            'cooking_time': 10
         }
-        json_data = json.dumps(data)
-        self.client.force_login(self.user)
-        response = self.client.post(url, data=json_data,
-                                    content_type='application/json')
+        response = self.client.post(url, data, format='json')
         self.assertEquals(status.HTTP_201_CREATED, response.status_code)
+        self.assertEquals(Recipe.objects.count(), 1)
+        self.assertEquals(Recipe.objects.get().name, 'Яичница')
